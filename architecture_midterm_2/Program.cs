d1b74b547e8e8dca81ce71c40ea2346b02f61418
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2024-03-06 15:22:00 +0200

    Initial commit

---
+++ architecture_midterm_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace architecture_midterm_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hey");
+        }
+    }
+}
